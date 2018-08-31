9e4f673e5dd6ba4889ab7f85359477a781a95b88
Shawn Salat <shawn.salat@gmail.com>
2018-08-31 15:16:06 -0700

    remove debugger

--- client/src/components/incident.jsx
+++ client/src/components/incident.jsx
@@ -7,11 +7,20 @@ export default class Incident extends React.Component {
   }
 
   incidentDetails(incident) {
+    let filter = ['sys_updated_by','sys_created_by', 'calendar_stc', 'made_sla', 'watch_list', 'upon_reject', 'sys_updated_on', 'hold_reason', 'caused_by', 'approval_history'];
+    let whiteList = ['parent', 'sys_updated_on', 'number', 'resolved_by', 'link', 'opened_by', 'closed_at', 'impact', 'active', 'priority','opened_at','resolved_at','category', 'subcategory', 'short_description', 'description', 'assignment_group', 'close_notes', 'closed_by', 'incident_state','urgency', 'assigned_to', 'severity','comments','approval','due_date', 'comments_and_work_notes', 'escalation', 'reopen_count','location'];
     return Object.keys(incident).map (key => {
       if (typeof incident[key] !== 'object'){
-        return <li>{key}: {incident[key]}</li>;
+        if (whiteList.includes(key) || incident[key].length < 1 ) {
+          return;
+        } else if (key !== 'link'){
+          return <li>{key}: {incident[key]}</li>;
+        } else {
+          return <li><Link to={incident[key]}>{key}</Link></li>;
+        }
       } else {
         return <ul>
+          <li>{key}</li>
           {this.incidentDetails(incident[key])}
         </ul>;
       }
--- client/src/components/incident_manager.jsx
+++ client/src/components/incident_manager.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactTable from "react-table";
+import ReactModal from 'react-modal';
 import Link from 'react-router-dom';
 import "react-table/react-table.css";
 
+import Incident from './incident';
+
 export default class IncidentManager extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
   openModal(state, rowInfo, column, instance) {
       if (column.Header === "Incident Number") {
         return {
@@ -11,7 +32,9 @@ export default class IncidentManager extends React.Component {
           let row = caches.incidents.filter( incident => {
             return incident.number === e.currentTarget.innerText;
           })[0];
-          this.props.history.push(`incidents/${row.number}`);
+
+          this.handleOpenModal();
+          // this.props.history.push(`incidents/${row.number}`);
         },
         style: {
           cursor: 'pointer'
@@ -24,6 +47,9 @@ export default class IncidentManager extends React.Component {
   render() {
     return(
       <div>
+        <ReactModal isOpen={this.state.showModal} appElement={document.getElementById('root')}>
+          <Incident />
+        </ReactModal>
         <ReactTable
           data={caches.incidents}
           columns={[
